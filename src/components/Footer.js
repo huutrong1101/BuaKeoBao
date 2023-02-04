957f3d851f9a5cf6d70853bedee1339ce511c720
Nguyễn Hữu Trọng <75382199+huutrong1101@users.noreply.github.com>
2023-02-04 16:39:18 +0700

    BuaKeoBao

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Modal from "./Modal";
+const Footer = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <>
+      <footer className="footer">
+        <div class="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a href="https://www.facebook.com/huutrong1101/" target="_blank">
+            Nguyễn Hữu Trọng
+          </a>
+          .
+        </div>
+        <button className="rules" onClick={toggle}>
+          Rules
+        </button>
+      </footer>
+      {modal ? <Modal toggle={toggle} /> : null}
+    </>
+  );
+};
+
+export default Footer;
